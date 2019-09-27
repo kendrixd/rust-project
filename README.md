324f7dee56750c14fa4e0e400910cada22183376
Kendrix <52902411+kendrixxx@users.noreply.github.com>
2019-09-27 22:40:13 +0800

    ws server

---
+++ README.md
@@ -0,0 +1,12 @@
+SERVER
+```bash
+cd examples/websocket
+cargo run --bin websocket-server
+# Started http server: 127.0.0.1:8080
+```
+RUST CLIENT
+
+```bash
+cd examples/websocket
+cargo run --bin websocket-client
+```
---
+++ src/main.rs
@@ -0,0 +1,77 @@
+use std::time::{Duration, Instant};
+use actix::prelude::*;
+use actix_files as fs;
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    println!("{:?}", r);
+    let res = ws::start(MyWebSocket::new(), &r, stream);
+    println!("{:?}", res.as_ref().unwrap());
+    res
+}
+
+struct MyWebSocket {
+    hb: Instant,
+}
+
+impl Actor for MyWebSocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.hb(ctx);
+    }
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for MyWebSocket {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        println!("WS: {:?}", msg);
+        match msg {
+            ws::Message::Ping(msg) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            ws::Message::Pong(_) => {
+                self.hb = Instant::now();
+            }
+            ws::Message::Text(text) => ctx.text(text),
+            ws::Message::Binary(bin) => ctx.binary(bin),
+            ws::Message::Close(_) => {
+                ctx.stop();
+            }
+            ws::Message::Nop => (),
+        }
+    }
+}
+
+impl MyWebSocket {
+    fn new() -> Self {
+        Self { hb: Instant::now() }
+    }
+    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                println!("Websocket Client heartbeat failed, disconnecting");
+                ctx.stop();
+                return;
+            }
+            ctx.ping("");
+        });
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/ws/").route(web::get().to(ws_index)))
+            .service(fs::Files::new("/", "static/"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+}
